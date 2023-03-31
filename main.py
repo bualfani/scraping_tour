b9241c08572d663cfeef80e9fce40194511bf637
bahati <bulozealfani@gmail.com>
2023-03-31 01:02:34 -0500

    added Event class and Email class

--- main.py
+++ main.py
@@ -23,45 +23,50 @@ class Event:
         extractor = selectorlib.Extractor.from_yaml_file('extract.yaml')
         value = extractor.extract(source)['tours']
 
+class Email:
+    def send(self, message):
+        host = 'smtp.gmail.com'
+        port = 465
+        username = 'your_email'
+        password = os.getenv("Password")
+        receiver = "your_email"
+        my_context = ssl.create_default_context()
 
-def send_email(message):
-    host = 'smtp.gmail.com'
-    port = 465
-    username = 'your_email'
-    password = os.getenv("Password")
-    receiver = "your_email"
-    my_context = ssl.create_default_context()
+        with smtplib.SMTP_SSL(host, port, context=my_context) as server:
+            server.login(username, password)
+            server.sendmail(username, receiver, message)
 
-    with smtplib.SMTP_SSL(host, port, context=my_context) as server:
-        server.login(username, password)
-        server.sendmail(username, receiver, message)
 
-def store(extrated):
-    row = extracted.split(",")
-    row = [item.strip() for item in row]
-    cursor = connection.cursor()
-    cursor.execute("INSERT INTO events VALUES(?,?,?)", row)
-    connection.commit()
+class Database:
+    def store(self, extrated):
+        row = extracted.split(",")
+        row = [item.strip() for item in row]
+        cursor = connection.cursor()
+        cursor.execute("INSERT INTO events VALUES(?,?,?)", row)
+        connection.commit()
 
-def read(extracted):
-    row = extracted.split(",")
-    row = [item.strip() for item in row]
-    Band, City, Date = row
-    cursor = connection.cursor()
-    cursor.execute("SELECT * FROM events WHERE Band=? City=? Date=?", (Band, City, Date))
-    rows = cursor.fetchall()
-    return rows
+    def read(self, extracted):
+        row = extracted.split(",")
+        row = [item.strip() for item in row]
+        Band, City, Date = row
+        cursor = connection.cursor()
+        cursor.execute("SELECT * FROM events WHERE Band=? City=? Date=?", (Band, City, Date))
+        rows = cursor.fetchall()
+        return rows
+
 
 if __name__ == "__main__":
     while True:
-        scraped = scrape(url)
-        extracted = extract(scraped)
+        event = Event()
+        scraped = event.scrape(url)
+        extracted = event.extract(scraped)
 
 
         if extracted != 'No upcoming tour':
             row = read(extracted)
             if not row:
                 store(extracted)
-                send_email(message="New Event was found")
+                email= Email()
+                email.send(message="New Event was found")
         # check for new event every two sec.
         time.sleep(2)
